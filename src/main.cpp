67ecca24c147c6c3db27f95ecda91baf7a61cbac
Jesse Morgan <morgajel@gmail.com>
2020-01-02 00:30:18 -0500

    saving the unreliable blynk code

--- src/main.cpp
+++ src/main.cpp
@@ -1,122 +1,103 @@
+#include <Arduino.h>
+#define BLYNK_PRINT Serial
 
-// #include <Arduino.h>
 #include <WiFi.h>
 #include <WiFiClient.h>
-// #include <WebServer.h>
-// #include <ESPmDNS.h>
-// #include "config.h"
+#include <WebServer.h>
+#include <ESPmDNS.h>
+#include <Servo.h>
+#include <BlynkSimpleEsp32.h>
 
-// // Initialize Constants
-// const int laserPin = 12;
-// const int panPin = 20;
-// const int tiltPin = 21;
-// const int serverPort = 80;
-// const int maxServoAngle = 180;
+#include "config.h"
 
-// // Volatile states
-// volatile int laserState = LOW;
-// volatile int currentTilt = 0;
-// volatile int currentPan = 0;
 
-// // Complex stuff
-// WebServer server(serverPort);
+// Initialize Constants
+const int laserPin = 11;
+const int panPin = 23;
+const int tiltPin = 22;
+
+// Volatile states
+volatile int laserState = LOW;
+volatile int currentTilt = 90;
+volatile int currentPan = 90;
+volatile int destTilt = 90;
+volatile int destPan = 90;
+volatile int stepSpeed = 10; // This is 10 ms/degree rotation
+
+
+Servo panServo;
+Servo tiltServo;
+
+BLYNK_WRITE(V2) {
+  int x = param.asInt();
+  digitalWrite(laserPin, x);
+  digitalWrite(laserPin, LOW);
+  Serial.println("Set Pin " + String(x));
+}
+
+BLYNK_WRITE(V3) {
+  int x = param[0].asInt();
+  int y = param[1].asInt();
+  panServo.write(x);
+  tiltServo.write(y);
+}
+BLYNK_WRITE(V4) {
+  int y = param.asInt();
+  tiltServo.write(y);
+}
+
+
 
 // // Prepare the Lasers
-// void laserOn(){
-//   digitalWrite(laserPin, HIGH);
-// }
+void laserOn(){
+   digitalWrite(laserPin, HIGH);
+}
 
-// void laserOff(){
-//   digitalWrite(laserPin, LOW);
-// }
-
-// // Lets Serve some Pages
-// void handleNotFound() {
-//   String message = "File Not Found\n\n";
-//   message += "URI: ";
-//   message += server.uri();
-//   message += "\nMethod: ";
-//   message += (server.method() == HTTP_GET) ? "GET" : "POST";
-//   message += "\nArguments: ";
-//   message += server.args();
-//   message += "\n";
-//   for (uint8_t i = 0; i < server.args(); i++) {
-//     message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
-//   }
-//   server.send(404, "text/plain", message);
-// };
+void laserOff(){
+   digitalWrite(laserPin, LOW);
+}
 
 
-// void connectToWifi(void){
-//   //Configure WIFI using config.h values
-//   WiFi.mode(WIFI_STA);
-//   WiFi.begin(ssid, password);
-//   while (WiFi.status() != WL_CONNECTED) {
-//     delay(500);
-//     Serial.print(".");
-//   }
+
+void connectToWifi(void){
+
+  Blynk.config(token);
+  Blynk.begin(token, ssid, password);
+
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
 
-//   Serial.println("");
-//   Serial.print("Connected to ");
-//   Serial.println(ssid);
-//   Serial.print("IP address: ");
-//   Serial.println(WiFi.localIP());
-
-
-//   if (MDNS.begin("esp32")) {
-//     Serial.println("MDNS responder started");
-//   }
+  if (MDNS.begin("esp32")) {
+    Serial.println("MDNS responder started");
+  }
   
-// }
+}
 
-
-// // Setup the initial values.
-// void setup(void) {
-//   // Set our Serial port rate
-//   Serial.begin(115200);
-//   // Prep our laser pin
-//   pinMode(laserPin, OUTPUT);
-//   // Wait for connection
-//   connectToWifi();
-
-// // Define all of these handlers for incoming URLs as anonymous lambda functions
-//   server.on("/", [](){
-//     String message = "Catdrainer 5000; now with more Servos";
-//     server.send(200, "text/plain", message);
-//   });
-
-//   server.on("/left", []() {
-//     server.send(200, "text/plain", "Turn Left");
-//   });
-  
-//   server.on("/laserOn", []() {
-//     server.send(200, "text/plain", "Fire da Laser PEW PEW PEW");
-//     laserOn();
-//   });
-  
-//   server.on("/laserOn", []() {
-//     server.send(200, "text/plain", "*terminator dies*");
-//     laserOff();
-//   });
-
-//   server.onNotFound(handleNotFound);
-
-//   server.begin();
-//   Serial.println("HTTP server started");
-// }
-
-// // The big bad loop in the sky
-// // Note, the server handleClient does all the heavy lifting.
-// void loop(void) {
-//   server.handleClient();
-// }
-
-void setup(void){
+// Setup the initial values.
+void setup(void) {
+  // Set our Serial port rate
   Serial.begin(115200);
+  // Prep our laser pin
+  pinMode(laserPin, OUTPUT);
+
+
+
+  panServo.attach(panPin);
+  tiltServo.attach(tiltPin);
+
+  // Wait for connection
+  Serial.println("Let's print some dots!");
+  connectToWifi();
+
 }
 
-void loop(void){
-  Serial.println("derp");
-}
 
+// The big bad loop in the sky
+// Note, the server handleClient does all the heavy lifting.
+void loop(void) {
+  Blynk.run();
+}
